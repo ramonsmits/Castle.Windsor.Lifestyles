ea44423d575cb67f59eccb4f8aae0666a4d171e7
Ramon Smits <ramon.smits@gmail.com>
2012-03-01 17:59:57 +0100

    Strong named assembly

--- Castle.Windsor.Lifestyles/Properties/AssemblyInfo.cs
+++ Castle.Windsor.Lifestyles/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.2")]
 [assembly: AssemblyFileVersion("0.1.0.2")]
-[assembly: InternalsVisibleTo("Castle.Windsor.Lifestyles.Tests")]
+[assembly: InternalsVisibleTo("Castle.Windsor.Lifestyles.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010093462f79bacd87df87ba18a0623ac8f767f0a251d83733e8bc80e4bd97d6ca72754fbd7f0d3d353ee37bd0bd3ba9ee96c4e768dd6b28ee68575fe179de84e44abb678bdeb733d9eff7ef4c3ddb7bbc8a35f2fb36064d2b70d107fcddcac5fac94228c50d0589d300df2c199d8940c5a700e2dab4b64bdab933207996d16065c1")]
